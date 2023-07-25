07f55872c36c962e64dbc8c918111b1f91cf326f
Humberto Nagato <humberto.nagatog@gmail.com>
2023-07-25 11:00:27 -0300

    add config.yml and tests

---
+++ calculator.py
@@ -0,0 +1,8 @@
+"""
+    Calculator library containing basic math operation
+"""
+def add(first_term,second_term):
+    return first_term + second_term
+
+def subtract(first_term,second_term):
+    return first_term - second_term
